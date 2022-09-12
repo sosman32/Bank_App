e99554e422c092a35793337d6f10bf10b4f41816
Suleman Osman <sosman32@hotmail.com>
2022-09-11 21:55:23 -0400

    Init commit

---
+++ my_bank_app/src/App.jsx
@@ -0,0 +1,39 @@
+import styles from './style';
+
+import {Navbar,Billing,CardDeal,Business,Clients,ClickToAction,Stats,Footer,Testimonials,Hero,
+} from "./components";
+
+const App = () => (
+    <div className="bg-primary w-full  overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          {/* This will be the nav bar section */}
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          {/* This will be the hero section, the main page where everything is initially loaded */}
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          {/* This will be several different sections displayed on page */}
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <ClickToAction />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+
+
+export default App
---
+++ my_bank_app/src/components/ClickToAction.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ClickToAction = () => {
+  return (
+    <div>ClickToAction</div>
+  )
+}
+
+export default ClickToAction
+
